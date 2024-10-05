8279626f21cccba95802591ca269d2aea9d8f12c
Skilvzlom <fedyakur1@gmail.com>
2024-10-05 22:17:26 +0300

    Completed)

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,14 @@
 package app.model;
 
-public class Timer {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component("Timer")
+public class Timer {
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
     }
+
 }
